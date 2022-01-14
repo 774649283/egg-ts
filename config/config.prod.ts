6dc886f1ad2d1bdb177691b5bb011a088d5d4b4d
刘抱 <liubao0000@163.com>
2022-01-14 16:16:48 +0800

    feat: prod config

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -1,6 +1,15 @@
 import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
-  const config: PowerPartial<EggAppConfig> = {};
+  const config = {} as PowerPartial<EggAppConfig>;
+  config.sequelize = {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    database: 'egg-wechat-prod',
+    username: '',
+    password: '',
+  };
+
   return config;
 };
